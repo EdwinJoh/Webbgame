031e833f6221d42e8a597ce89029f41597bcad34
Edwin Johansson <edwin.johansson@elev.nbi-handelsakademin.se>
2022-11-02 07:24:56 +0100

    update

---
+++ Contracts/IWeaponRepository.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IWeaponRepository
+    {
+        Task<Weapon> GetWeapon(string name,bool trackChanges);
+    }
+}
---
+++ Repository/WeaponRepository.cs
@@ -0,0 +1,20 @@
+﻿using Contracts;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository;
+
+public class WeaponRepository:RepositoryBase<Weapon>,IWeaponRepository
+{
+	public WeaponRepository(RepositoryContext context) : base(context)
+	{
+
+	}
+	public async Task<Weapon> GetWeapon(string name, bool trackChanges) =>
+		await FindByCondition(x => x.Name.Equals(name),trackChanges).FirstOrDefaultAsync();
+}
